ae8025a2fcb23b1cb94f012f9efef99e024eddf4
Krisna Sirait <krisnapande00@outlook.com>
2022-03-26 16:58:20 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/chatify/app/chatify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chatify.app.chatify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/splash_pages.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chatify',
+      theme: ThemeData(
+        backgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+        scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+      ),
+      home: Scaffold(
+          body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/chat.png'),
+            ),
+          ),
+        ),
+      )),
+    );
+  }
+}
